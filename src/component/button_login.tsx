60db49b240ef080c85f7c65eb4be82aeb3457754
한규현 <tx.seol@gmail.com>
2025-10-30 15:39:24 +0900

    버튼 실제 컴포넌트 사용 및 css 일부사용, gps에러메시지 주석처리

--- src/component/button_login.tsx
+++ src/component/button_login.tsx
@@ -9,7 +9,10 @@ export default function ButtonLogin() {
   };
 
   return (
-    <button className="bg-blue-500" onClick={handleLogin}>
+    <button
+      className="bg-blue-500 text-white rounded-lg p-1"
+      onClick={handleLogin}
+    >
       로그인
     </button>
   );
