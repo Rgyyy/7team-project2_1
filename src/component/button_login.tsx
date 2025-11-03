c9a34b89547b66aec680b175a223bc32d01a1b79
한규현 <tx.seol@gmail.com>
2025-11-03 14:20:20 +0900

    프로필페이지 생성, 버튼컴포넌트들 호버추가

--- src/component/button_login.tsx
+++ src/component/button_login.tsx
@@ -10,7 +10,7 @@ export default function ButtonLogin() {
 
   return (
     <button
-      className="bg-purple-600 text-white rounded-lg p-1"
+      className="bg-purple-600 hover:bg-purple-700 text-white rounded-lg p-1"
       onClick={handleLogin}
     >
       로그인
